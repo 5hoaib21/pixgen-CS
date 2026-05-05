3308803427d2f21cc25d99b60d0b2ac87bbf96f2
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-06 02:56:26 +0600

    added category filter sections

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
+const Category = async () => {
+  const res = await fetch('https://pixgen-cs.vercel.app/category.json')
+  const data = await res.json()
+  // console.log(data);
+  return (
+    <div className="flex gap-3">
+      {
+        data.map(category => <Link key={category.id} href={`?category=${category.name.toLowerCase()}`}><Button variant="outline" >{category.name}</Button></Link>)
+      }
+    </div>
+  );
+};
+
+export default Category;
